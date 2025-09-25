9e9d808333460020ae35acc1266421f474208892
HuiBeom <gmlqja7913@daum.net>
2025-09-25 18:59:23 +0900

    todoList-toggle, delete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -14,9 +14,25 @@ const TodoItem = ({item}) => {
         setTodoList(newList);
     }
 
+    const toggleItemCompletion = () => {
+        const newList = replaceItemAtIndex(todoList, index, {
+            ...item,
+            isComplete: !item.isComplete
+        })
+        setTodoList(newList);
+    }
+
+    const deleteItem = () => {
+        const newList = removeItemAtIndex(todoList, index);
+
+        setTodoList(newList);
+    }
+
     return (
         <div>
             <input type="text" value={item.text} onChange={editItemText}/>
+            <input type="checkbox" checked={item.isComplete} onChange={toggleItemCompletion} />
+            <button onClick={deleteItem}>X</button>
         </div>
     );
 };
@@ -25,4 +41,8 @@ export default TodoItem;
 
 function replaceItemAtIndex(arr, index, newValue) {
     return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function  removeItemAtIndex(arr, index){
+    return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
