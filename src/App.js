1699a8c70bde94f42d5e8d54a0046b1d763a1adb
HuiBeom <gmlqja7913@daum.net>
2025-09-25 18:51:11 +0900

    todo-update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoItemCreator from "./components/TodoItemCreator";
+import {useRecoilValue} from "recoil";
+import {todoListState} from "./todoAtom";
+import TodoItem from "./components/TodoItem";
 
 function App() {
+    const todoList = useRecoilValue(todoListState)
+
   return (
     <div className="App">
       <TodoItemCreator />
+        {todoList.map((todoItem) => (
+            <TodoItem key={todoItem.id} item={todoItem} />
+        ))}
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useRecoilState} from "recoil";
+import {todoListState} from "../todoAtom";
+
+const TodoItem = ({item}) => {
+    const [todoList, setTodoList] = useRecoilState(todoListState);
+    const index = todoList.findIndex((listItem) => listItem === item);
+
+    const editItemText = ({target : {value}}) => {
+        const newList = replaceItemAtIndex(todoList, index, {
+            ...item,
+            text: value
+        })
+        setTodoList(newList);
+    }
+
+    return (
+        <div>
+            <input type="text" value={item.text} onChange={editItemText}/>
+        </div>
+    );
+};
+
+export default TodoItem;
+
+function replaceItemAtIndex(arr, index, newValue) {
+    return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
