930bdcccf9701bfd9a31f11e014cd47e1b4dd3e6
HuiBeom <gmlqja7913@daum.net>
2025-09-25 19:22:13 +0900

    filteredTodoList

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItemCreator from "./components/TodoItemCreator";
 import {useRecoilValue} from "recoil";
-import {todoListState} from "./todoAtom";
+import {filteredTodoListState, todoListState} from "./todoAtom";
 import TodoItem from "./components/TodoItem";
+import TodoListFilters from "./components/TodoListFilters";
 
 function App() {
     const todoList = useRecoilValue(todoListState)
+    const filteredTodoList = useRecoilValue(filteredTodoListState)
 
   return (
     <div className="App">
+      <TodoListFilters />
       <TodoItemCreator />
-        {todoList.map((todoItem) => (
-            <TodoItem key={todoItem.id} item={todoItem} />
-        ))}
+      {filteredTodoList.map((todoItem) => (
+          <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
     </div>
   );
 }
