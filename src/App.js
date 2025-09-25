47888760527b73b084b1deb151b639f3b1342790
HuiBeom <gmlqja7913@daum.net>
2025-09-26 01:37:00 +0900

    통계 기능, 비동기 데이터 쿼리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {useRecoilValue} from "recoil";
 import {filteredTodoListState, todoListState} from "./todoAtom";
 import TodoItem from "./components/TodoItem";
 import TodoListFilters from "./components/TodoListFilters";
+import TodoListStats from "./components/TodoListStats";
+import {currentUserNameQuery} from "./userAtoms";
+import {Suspense} from "react";
 
 function App() {
     const todoList = useRecoilValue(todoListState)
@@ -12,6 +15,10 @@ function App() {
 
   return (
     <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+          <CurrentUserInfo />
+      </Suspense>
+      <TodoListStats />
       <TodoListFilters />
       <TodoItemCreator />
       {filteredTodoList.map((todoItem) => (
@@ -22,3 +29,8 @@ function App() {
 }
 
 export default App;
+
+function CurrentUserInfo() {
+    const userName = useRecoilValue(currentUserNameQuery);
+    return <div>{userName}</div>
+}
